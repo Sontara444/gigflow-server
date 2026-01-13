0f0f35d371bf04c522ee32650fc725b4ea88cb50
Sontara444 <sontararajput@gmail.com>
2026-01-13 18:40:41 +0530

    update feature

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { sendMessage, getMessages, getConversations } = require('../controllers/messageController');
+const { sendMessage, getMessages, getConversations, markMessagesRead } = require('../controllers/messageController');
 
 const router = express.Router();
 
 router.post('/', protect, sendMessage);
+router.put('/read/:senderId', protect, markMessagesRead);
 router.get('/:userId', protect, getMessages);
 router.get('/', protect, getConversations);
 
